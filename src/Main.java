d736cc98e5c3d8c63474b8f61c3afd18f9a6ed30
Stepan Smirnov <stechkin1986@gmail.com>
2020-10-12 23:30:21 +0300

    modified report.md

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        long current;
+        int current;
         current = 2_000_000_000;
         int transfer;
         transfer = 500_000_000;
