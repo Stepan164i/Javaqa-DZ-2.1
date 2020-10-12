ef4b7be1ecbe4872c2dc3ba6925788ab93a44b9c
Stepan Smirnov <stechkin1986@gmail.com>
2020-10-12 19:08:49 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        long current;
+        current = 2_000_000_000;
+        int transfer;
+        transfer = 500_000_000;
+        long total;
+        total = current + transfer;
+        System.out.println(total);
+
+    }
+}
+
